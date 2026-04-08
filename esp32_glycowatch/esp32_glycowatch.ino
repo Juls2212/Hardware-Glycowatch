e581c4db62e753565fa50a229e55def4def130b6
Laura <laura.escobarca@campusucc.edu.co>
2026-04-08 13:04:38 -0500

    Modificaciones ESP32

--- esp32_glycowatch/esp32_glycowatch.ino
+++ esp32_glycowatch/esp32_glycowatch.ino
@@ -1,13 +1,23 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 
+// WIFI
 const char* ssid = "ESCOBAR-2G";
-const char* password = "PASSWORD-WIFI";
+const char* password = "WIFI-PASSWORD";
 
+// MQTT
 const char* mqtt_server = "test.mosquitto.org";
+const char* topic = "glycowatch/device/ESP32-ABC-123";
+
+// DEVICE DATA
+const char* deviceIdentifier = "ESP32-ABC-123";
+const char* apiKey = "zUExsMdJ23LHBySUW69o37vie4ZbVmH3fzXZA-mJTKU"; // REEMPLAZAR
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+// contador para evitar duplicados
+int counter = 0;
 
 void setup_wifi() {
   delay(10);
@@ -40,9 +50,7 @@ void reconnect() {
 
 void setup() {
   Serial.begin(115200);
-
   setup_wifi();
-
   client.setServer(mqtt_server, 1883);
 }
 
@@ -52,23 +60,26 @@ void loop() {
   }
   client.loop();
 
-  int glucose = random(80, 200); // simulación
+  int glucose = random(80, 200);
+
+  counter++;
+
+  String sourceEventId = "esp32-" + String(counter);
 
   String payload = "{";
-  payload += "\"deviceId\": \"10\",";
-  payload += "\"glucoseMgDl\": " + String(glucose) + ",";
-  payload += "\"measuredAt\": \"2026-04-07T12:00:00Z\"";
+  payload += "\"deviceIdentifier\":\"" + String(deviceIdentifier) + "\",";
+  payload += "\"apiKey\":\"" + String(apiKey) + "\",";
+  payload += "\"glucoseMgDl\":" + String(glucose) + ",";
+  payload += "\"measuredAt\":\"2026-04-07T12:00:00Z\",";
+  payload += "\"sourceEventId\":\"" + sourceEventId + "\",";
+  payload += "\"origin\":\"esp32-mqtt\"";
   payload += "}";
 
-  client.publish("glycowatch/device/10", payload.c_str());
+  client.publish(topic, payload.c_str(), false);
 
   Serial.println("Enviado:");
-  Serial.println("{");
-  Serial.println("  \"deviceId\": \"10\",");
-  Serial.println("  \"glucoseMgDl\": " + String(glucose) + ",");
-  Serial.println("  \"measuredAt\": \"2026-04-07T12:00:00Z\"");
-  Serial.println("}");
+  Serial.println(payload);
   Serial.println("----------------------");
 
-  delay(5000);
+  delay(10000); // cada 10 segundos
 }
