010cf411b2bd675af71a09f896eb1addd58860ce
Laura <laura.escobarca@campusucc.edu.co>
2026-05-28 17:51:36 -0500

    integrar configuracion MQTT con HiveMQ Cloud

--- esp32_glycowatch/esp32_glycowatch.ino
+++ esp32_glycowatch/esp32_glycowatch.ino
@@ -1,10 +1,26 @@
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
 
 /* =========================
    WIFI CONFIG
 ========================= */
 const char* WIFI_SSID = "WIFI_UCC_ESTUDIANTES";
 const char* WIFI_PASSWORD = "Estudiantes.2026*";
+
+/* =========================
+   HIVEMQ CLOUD CONFIG
+========================= */
+const char* MQTT_BROKER =
+"478aa6d5a50348a285de276f76d74233.s1.eu.hivemq.cloud";
+
+const int MQTT_PORT = 8883;
+
+const char* MQTT_USERNAME = "glycowatch_backend";
+const char* MQTT_PASSWORD = "glycoProyecto2212";
+
+WiFiClientSecure secureClient;
+PubSubClient mqttClient(secureClient);
 
 void connectWiFi() {
   Serial.println();
