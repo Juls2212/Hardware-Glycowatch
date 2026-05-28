b78230e99382fb5cd17b72da1dd799651026c19b
Laura <laura.escobarca@campusucc.edu.co>
2026-05-28 17:50:25 -0500

    agregar logs seriales para monitoreo del sistema

--- esp32_glycowatch/esp32_glycowatch.ino
+++ esp32_glycowatch/esp32_glycowatch.ino
@@ -7,15 +7,28 @@ const char* WIFI_SSID = "WIFI_UCC_ESTUDIANTES";
 const char* WIFI_PASSWORD = "Estudiantes.2026*";
 
 void connectWiFi() {
+  Serial.println();
+  Serial.println("==================================");
+  Serial.println("CONECTANDO A WIFI...");
+  Serial.println("==================================");
+
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
   while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
     delay(500);
   }
+
+  Serial.println();
+  Serial.println("WIFI CONECTADO!");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void setup() {
+  Serial.begin(115200);
+
   connectWiFi();
 }
 
